05426f93342c43fff816881c94c44104b95c2744
Elliot Tuck <eat3774@gmail.com>
2018-01-15 18:00:24 -0500

    Update animation framework
    Update the animation framework to test the injection functionality.

--- animation.py
+++ animation.py
@@ -1,40 +1,49 @@
-import random
-import time
 import math
-
+from injection import *
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
+# create new injection
+injection = Injection(100, 0.05, 0.1)
 
-class RegrMagic(object):
-    """Mock for function Regr_magic()
-    """
+"""
+Data Generator class. Initialize with a base value, and then repeatedly update the values
+that are generated.
+"""
+class DataGenerator(object):
     def __init__(self):
         self.x = 0
 
     def __call__(self):
-        self.x += 0.1
-        return self.x, math.sin(self.x)
+        self.x += 1
+        if self.x == 140 or self.x == 180:   # sample injections
+            injection.inject()
+        # add "real data" (represented as a smooth sine function here) with injected data
+        return self.x, math.sin(self.x / 10.0) + injection.get_offset()   
 
-regr_magic = RegrMagic()
+# create new DataGenerator
+data_gen = DataGenerator()
 
+# this function repeatedly reads the next available data point to plot
 def frames():
     while True:
-        yield regr_magic()
+        yield data_gen()
 
+# create a figure to plot on and two arrays to store the axis values
 fig = plt.figure()
-
 x = []
 y = []
 
+# this function gets called every interval milliseconds to update the plot
 def animate(args):
     x.append(args[0])
     y.append(args[1])
-    if len(x) > 100:
+    if len(x) > 100:   # remove old data points so plot "scrolls" along
         fig.clf()
         x.pop(0)
         y.pop(0)
     return plt.plot(x, y, color='g')
 
+# start the visualization
 anim = animation.FuncAnimation(fig, animate, frames=frames, interval=100)
 plt.show()

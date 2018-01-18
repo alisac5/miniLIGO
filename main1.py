dfbe80a225aec82f5aa7f4c227cd6b92a50d3081
hsnee <husni@physics.org>
2018-01-18 06:50:32 -0500

    still working on injection

--- main1.py
+++ main1.py
@@ -10,8 +10,7 @@ def calibrate(lux, full_wavelength):
 def inject():
     x = np.linspace(0,2,21)
     y = np.sin(x**2)*x*10**-8
-    for i in range()
-
+    return x,y
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
@@ -19,8 +18,9 @@ i = 0
 plt.figure()
 plt.ion()
 import tsl2591
-from audiolizer import main as audiolize
+#from audiolizer import main as audiolize
 lst = []
+lstx = []
 luxes = []
 full_wavelength = 0
 tsl = tsl2591.Tsl2591()
@@ -28,6 +28,7 @@ while True:
     # check for trigger
     input_state = GPIO.input(18)
     if input_state == False:
+        plt.figure()
         x,y = inject()
         lsti = []
         lstj = []
@@ -36,21 +37,24 @@ while True:
             lstj.append(j)
             plt.plot(lsti,lstj)
             time.sleep(0.1)
-
+    full, ir = tsl.get_full_luminosity()
+    lux = tsl.calculate_lux(full, ir)
     strain = calibrate(lux,full_wavelength)
 
   # plot strain as y-data point
     if len(lst)>50:
+        del lstx[0] 
         del lst[0]
     lst.append(strain)
-    plt.plot(i, lst)
-    i+=0.1
+    lstx.append(i)
+    plt.plot(lstx, lst)
+    i+=1
     plt.show()
     plt.pause(0.01)
     plt.xlim((i-10,i+10))
 
   # send to audio
-    audiolize(strain/100)
+  #  audiolize(strain/100)
 
 while True:
     plt.pause(0.05)

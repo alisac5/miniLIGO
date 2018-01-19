fd0a170e2ab1dfea6890e6706996ba8ceb9dbd5c
Husni Almoubayyed <husni@physics.org>
2018-01-19 11:16:02 -0500

    adding button support

--- main1.py
+++ main1.py
@@ -3,8 +3,17 @@ sns.set_style('darkgrid')
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-def calibrate(lux):
-    return np.sin(np.random.rand())
+import RPi.GPIO as GPIO
+
+def calibrate(lux, full_wavelength):
+    return full_wavelength*10**-9*530 + np.random.randn()*lux*10**-9*100
+def inject():
+    x = np.linspace(0,2,21)
+    y = np.sin(x)*x
+    for i in range()
+
+PIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 i = 0
 plt.figure()
@@ -12,11 +21,18 @@ plt.ion()
 import tsl2591
 from audiolizer import main as audiolize
 lst = []
+luxes = []
+full_wavelength = 0
 tsl = tsl2591.Tsl2591()
 while True:
-    full, ir = tsl.get_full_luminosity()
-    lux = tsl.calculate_lux(full,ir)
-    strain = calibrate(lux)
+    # check for trigger
+    input_state = GPIO.input(18)
+    if input_state == False:
+        inject()
+
+
+
+    strain = calibrate(lux,full_wavelength)
 
   # plot strain as y-data point
     if len(lst)>50:
@@ -26,10 +42,10 @@ while True:
     i+=0.1
     plt.show()
     plt.pause(0.01)
-    plt.xlim((i*4/10-10,i*4/10+10))
+    plt.xlim((i-10,i+10))
 
   # send to audio
-    # audiolize(strain)
+    audiolize(strain/100)
 
 while True:
     plt.pause(0.05)
